e74291fb22c260645c75ea62f6d8bfb3b6d9e417
YunMin-Song <thd930308@naver.com>
2020-10-27 23:45:46 +0900

    print Ip address Update

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from ipware.ip import get_client_ip
 
 
 # Create your views here.
 def home(request):
     # render : 첫번째 인자부터 요청, 템플릿 이름, 딕셔너리형 자료형
-    return render(request, 'home.html')
+    return render(request, 'home.html',{
+        'ipAddress': get_client_ip(request)
+    })
+
+
+# def ipAddress(request):
+#     ip = get_client_ip(request)
+#     return render(request, 'home.html', {
+#         'ipAddress': ip
+#     })
 
 
 def about(request):
@@ -14,17 +24,18 @@ def about(request):
 def result(request):
     # text 변수에 텍스트 받음
     text = request.GET['fulltext']
-    words = text.split() # split : 공백 기준으로 나눔
+    words = text.split()  # split : 공백 기준으로 나눔
     word_dictionary = {}
 
-    for word in words :
+    for word in words:
         if word in word_dictionary:
             word_dictionary[word] += 1
-        else :
-            #add to dictionary
+        else:
+            # add to dictionary
             word_dictionary[word] = 1
     # 세번째 인자 키:값
     return render(request, 'result.html',
                   {'full': text, 'total': len(words),
                    'dictionary': word_dictionary.items()})
+
 
